07275b14106fb1dac47345bbc2e246ba24bfc0f6
UnknownUserIDE <94220170+UnknownUserIDE@users.noreply.github.com>
2024-06-03 21:41:57 +0300

    Settings and updated icon usage

--- app/src/main/java/com/example/carrental_1/MainActivity.java
+++ app/src/main/java/com/example/carrental_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.carrental_1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -155,8 +156,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about_me) {
+            Intent intent = new Intent(this, SettingsActivity.class); // You might want to rename this to AboutMeActivity
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
